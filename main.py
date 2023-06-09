575ccc42fd8a5b2fa3c753b1a85bb6ebcc583fd0
barrak2018 <joseestebanojedaaular@gmail.com>
2023-06-09 19:55:47 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+
+# Programación Númerica y No Númerica DCN-501
+
+# Jorge Polo
+# C.I: V-18.587.447
+
+# Gustavo Hernández
+# C.I: V-28.317.635
+
+# Jose Ojeda
+# C.I: V-29.845.213
+
+# Leonardo Yanez
+# C.I: V-30.300.275
+import matplotlib.pyplot as pp
+
+# funciones
+def MuestraTitulo ():
+    print ("")
+
+def linearFuntion(dependiente = 1, independiente = 0):
+    x = list(range (-2, 3, 1))
+    y = list (map(lambda x : (x* dependiente + independiente), x))
+    tabla =  list(zip(x,y))
+    for index, tupla in enumerate (tabla):
+        x, y = tupla 
+        print(f"{index+1}: X= {x} Y= {y}")
+    pp.plot(x,y)
+    pp.show()
+    
+
+
+
+# main
+
+linearFuntion(9,12)
