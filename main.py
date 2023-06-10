9d773a680b4af7d8a6538bcfcf12d541e55e2d82
barrak2018 <joseestebanojedaaular@gmail.com>
2023-06-09 23:09:48 -0400

    final 1.0

--- main.py
+++ main.py
@@ -13,24 +13,64 @@
 # Leonardo Yanez
 # C.I: V-30.300.275
 import matplotlib.pyplot as pp
+import os
+
 
 # funciones
 def MuestraTitulo ():
-    print ("")
+    if os.name == "posix":
+        var = "clear"       
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+        var = "cls"
+    os.system(var)
+    tittle = "  _____                        .__  .__                     .__   \n_/ ____\_ __  ____             |  | |__| ____   ____ _____  |  |  \n\   __\  |  \/    \    ______  |  | |  |/    \_/ __ \ \__ \ |  |  \n |  | |  |  /   |  \  /_____/  |  |_|  |   |  \  ___/ / __ \|  |__\n |__| |____/|___|  /           |____/__|___|  /\___  >____  /____/\n                 \/                         \/     \/     \/      "
+    print(tittle)
 
 def linearFuntion(dependiente = 1, independiente = 0):
     x = list(range (-2, 3, 1))
     y = list (map(lambda x : (x* dependiente + independiente), x))
     tabla =  list(zip(x,y))
+    MuestraTitulo()
+    print('\nValores de coordenadas')
     for index, tupla in enumerate (tabla):
         a, b = tupla 
         print(f"{index+1}: X= {a} Y= {b}")
-    print (x, y)
+    
     pp.plot(x, y)
+    pp.xlabel("eje X")
+    pp.ylabel('eje Y')
+    pp.title("funcion lineal")
+    pp.grid()
+    print('cerrar grafica para terminar')
     pp.show()
 
+def MuestraMenu():
+    menu = f"1: salir \n\r2: funcion lineal"
+    print(menu)
 
 
 # main
 
-linearFuntion(9,12)
+while (True):
+    MuestraTitulo()
+    MuestraMenu()
+
+    option = int(input("==>  "))
+    
+
+
+    if option == 1:
+        break
+    elif option == 2:
+        
+        try:
+            MuestraTitulo()
+            a = int(input('favor de ingresar la variable dependiente:  '))
+            b = int(input('favor de ingresar la variable independiente:  '))
+            linearFuntion(a, b)
+        except:
+            print('Err de tipado')
+            input('enter para continuar')
+    else:
+        print('Error')
+        input('enter para continuar')
